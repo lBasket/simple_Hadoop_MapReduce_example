3138019f03cbe838177ab3b4eb68674bbe27630e
Trevor Carley <trevor.carley@gmail.com>
2021-09-04 01:05:22 +0000

    lowercase, some stopwords, and punctation removed

--- mapper.py
+++ mapper.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import sys
 
+stops = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves']
+
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-
+    line = line.replace(",", " ").replace(". ", " ").replace("-", " ")
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stops:
+            print '%s\t%s' % (word, "1")
